270fc8ea2232bfea7a6846442ff207ebe670ad11
BingLingGroup <42505588+BingLingGroup@users.noreply.github.com>
2026-09-11 10:56:18 +0300

    Add A1_T3.py A1_T4.py

---
+++ A1_T3.py
@@ -0,0 +1,2 @@
+name_string = input("What is your name: ")
+print("Hi there {Name}".format(Name=name_string))
---
+++ A1_T4.py
@@ -0,0 +1,9 @@
+Num1 = 47
+Num2 = 102
+Sum = Num1 + Num2
+Diff = Num2 - Num1
+Product = Sum * Diff
+print("{Num1} + {Num2} = {Sum}".format(Num1=Num1, Num2=Num2, Sum=Sum))
+print("{Num2} - {Num1} = {Diff}".format(Num1=Num1, Num2=Num2, Diff=Diff))
+print("{Sum} * {Diff} = {Product}".format(Sum=Sum, Diff=Diff, Product=Product))
+print("({Num1} + {Num2}) * ({Num2} - {Num1}) = {Product}".format(Num1=Num1, Num2=Num2, Product=Product))
