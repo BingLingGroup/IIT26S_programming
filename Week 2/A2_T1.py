35d8671482b1e58ec92c51534c0b55dfa270bad1
BingLingGroup <42505588+BingLingGroup@users.noreply.github.com>
2026-09-21 10:19:23 +0300

    fix: rounding issues in A2_T1.py, A2_T4.py, A2_T7.py

--- Week 2/A2_T1.py
+++ Week 2/A2_T1.py
@@ -2,8 +2,8 @@ print("Program starting.")
 name = input("What is your name: ")
 first_float = float(input("Enter a floating point number: "))
 second_float = float(input("Enter second floating point number: "))
-print("John you gave numbers {first:.2f} and {second:.2f}".format(
+print("John you gave numbers {first} and {second}".format(
     first=first_float, second=second_float))
-print("Multiplying first and second number will result in product {product:.2f}".format(
-    product=first_float * second_float))
+print("Multiplying first and second number will result in product {product}".format(
+    product=round(first_float * second_float, 2)))
 print("Program ending.")
--- Week 2/A2_T7.py
+++ Week 2/A2_T7.py
@@ -1,6 +1,6 @@
 print("Program starting.")
 Fahrenheit = float(input("Insert fahrenheits: "))
 Celcius = (Fahrenheit - 32) / 1.8
-print("{fahrenheit:.1f}°F is {celcius:.1f}°C".format(
-    fahrenheit=Fahrenheit, celcius=Celcius))
+print("{fahrenheit}°F is {celcius}°C".format(
+    fahrenheit=round(Fahrenheit, 1), celcius=round(Celcius, 1)))
 print("Program ending.")
